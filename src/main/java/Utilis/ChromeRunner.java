bf3b5ce7ac3f9fc55368e2e0833a7602c09a6b5e
TarielTopuria <tatotophuria@gmail.com>
2023-02-07 00:16:57 +0400

    setting up the environment

--- src/main/java/Utilis/ChromeRunner.java
+++ src/main/java/Utilis/ChromeRunner.java
@@ -1,2 +1,21 @@
-package Utilis;public class ChromeRunner {
+package Utilis;
+
+import DataObject.Common;
+import com.codeborne.selenide.Selenide;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class ChromeRunner implements Common {
+    @BeforeTest(description = "Opening browser with BASEURL before each test")
+    public static void chromeSetUp(){
+        open(BASEURL);
+    }
+
+    @AfterTest(description = "Clearing cookies and other unimportant files after each test")
+    public static void clearCookies(){
+        Selenide.clearBrowserCookies();
+        Selenide.clearBrowserLocalStorage();
+    }
 }
