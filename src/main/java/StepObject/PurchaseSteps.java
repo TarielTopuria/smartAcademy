27804a23cdd605e7a44aaa8570b0ea5b90c329d8
TarielTopuria <tatotophuria@gmail.com>
2023-02-18 13:49:03 +0400

    Added 26-34 Test Cases

--- src/main/java/StepObject/PurchaseSteps.java
+++ src/main/java/StepObject/PurchaseSteps.java
@@ -9,10 +9,97 @@ public class PurchaseSteps implements PurchasePage {
         sendBtn.click();
         return this;
     }
+    @Step("Get name field border color")
+    public String getNameFieldColor(){
+        return nameInputField.getCssValue("border-color");
+    }
+
+    @Step("Get code field border color")
+    public String getCodeFieldColor(){
+        return codeInputField.getCssValue("border-color");
+    }
+
+    @Step("Get address field border color")
+    public String getAddressFieldColor(){
+        return addressInputField.getCssValue("border-color");
+    }
+
+    @Step("Get phone field border color")
+    public String getPhoneFieldColor(){
+        return phoneInputField.getCssValue("border-color");
+    }
+
+    @Step("Get date field border color")
+    public String getDateFieldColor(){
+        return dateInputField.getCssValue("border-color");
+    }
+
+    @Step("Get email field border color")
+    public String getEmailFieldColor(){
+        return emailInputField.getCssValue("border-color");
+    }
+
+    @Step("Get code field value")
+    public String getCodeFieldValue(){
+        return codeInputField.getAttribute("value");
+    }
+
+    @Step("Get phone field value")
+    public String getPhoneFieldValue(){
+        return phoneInputField.getAttribute("value");
+    }
 
     @Step("Fill the \"კომპანიის სახელწოდება\" field")
     public PurchaseSteps fillNameField(){
         nameInputField.sendKeys("Test Name");
+        return this;
+    }
+
+    @Step("Input string in the \"კომპანიის სახელწოდება\" field")
+    public PurchaseSteps inputStringInCode(){
+        codeInputField.sendKeys("Test");
+        return this;
+    }
+
+    @Step("Input less than 11 digits in the \"კომპანიის სახელწოდება\" field")
+    public PurchaseSteps inputLessDigitsInCode(){
+        codeInputField.sendKeys("0123456789");
+        return this;
+    }
+
+    @Step("Input 12 digits in the \"კომპანიის სახელწოდება\" field")
+    public PurchaseSteps inputTwelveDigitsInCode(){
+        codeInputField.sendKeys("012345678912");
+        return this;
+    }
+
+    @Step("Input 11 digits in the \"კომპანიის სახელწოდება\" field")
+    public PurchaseSteps inputElevenDigitsInCode(){
+        codeInputField.sendKeys("01234567891");
+        return this;
+    }
+
+    @Step("Fill the \"ფაქტიური მისამართი\" field")
+    public PurchaseSteps fillAddressField(){
+        addressInputField.sendKeys("1, Test address");
+        return this;
+    }
+
+    @Step("Input string in the \"საკონტაქტო ნომერი\" field")
+    public PurchaseSteps inputStringInPhone(){
+        phoneInputField.sendKeys("Test");
+        return this;
+    }
+
+    @Step("Input less than 9 digits in the \"საკონტაქტო ნომერი\" field")
+    public PurchaseSteps inputLessDigitsInPhone(){
+        phoneInputField.sendKeys("01234567");
+        return this;
+    }
+
+    @Step("Input 12 digits in the \"საკონტაქტო ნომერი\" field")
+    public PurchaseSteps inputTwelveDigitsInPhone(){
+        phoneInputField.sendKeys("012345678912");
         return this;
     }
 }
--- src/test/java/PurchaseTest.java
+++ src/test/java/PurchaseTest.java
@@ -30,52 +30,129 @@ public class PurchaseTest extends ChromeRunner implements PurchasePage {
     public void checkNameFieldColor(){
         steps.clickSendBtn();
         sleep(400);
-        Assert.assertEquals(nameInputField.getCssValue("border-color"), "rgb(255, 0, 0)", "Verify if the field is empty, \"კომპანიის სახელწოდება\" turns red.");
+        Assert.assertEquals(steps.getNameFieldColor(), "rgb(255, 0, 0)", "Verify if the field is empty, \"კომპანიის სახელწოდება\" turns red.");
     }
 
     @Test(priority = 2)
     @Severity(SeverityLevel.NORMAL)
     @Description("Negative test case: Checking that if the field is empty, \"საიდენტიფიკაციო კოდი\" turns red.")
     public void checkCodeFieldColor(){
-        Assert.assertEquals(codeInputField.getCssValue("border-color"), "rgb(255, 0, 0)", "Verify if the field is empty, \"საიდენტიფიკაციო კოდი\" turns red.");
+        Assert.assertEquals(steps.getCodeFieldColor(), "rgb(255, 0, 0)", "Verify if the field is empty, \"საიდენტიფიკაციო კოდი\" turns red.");
     }
 
     @Test(priority = 3)
     @Severity(SeverityLevel.NORMAL)
     @Description("Negative test case: Checking that if the field is empty, \"ფაქტიური მისამართი\" turns red.")
     public void checkAddressFieldColor(){
-        Assert.assertEquals(addressInputField.getCssValue("border-color"), "rgb(255, 0, 0)", "Verify if the field is empty, \"ფაქტიური მისამართი\" turns red.");
+        Assert.assertEquals(steps.getAddressFieldColor(), "rgb(255, 0, 0)", "Verify if the field is empty, \"ფაქტიური მისამართი\" turns red.");
     }
 
     @Test(priority = 4)
     @Severity(SeverityLevel.NORMAL)
     @Description("Negative test case: Checking that if the field is empty, \"საკონტაქტო ნომერი\" turns red.")
     public void checkPhoneFieldColor(){
-        Assert.assertEquals(phoneInputField.getCssValue("border-color"), "rgb(255, 0, 0)", "Verify if the field is empty, \"საკონტაქტო ნომერი\" turns red.");
+        Assert.assertEquals(steps.getPhoneFieldColor(), "rgb(255, 0, 0)", "Verify if the field is empty, \"საკონტაქტო ნომერი\" turns red.");
     }
 
     @Test(priority = 5)
     @Severity(SeverityLevel.NORMAL)
     @Description("Negative test case: Checking that if the field is empty, \"როდის გსურთ შეკვეთის მიღება?\" turns red.")
     public void checkDateFieldColor(){
-        Assert.assertEquals(dateInputField.getCssValue("border-color"), "rgb(255, 0, 0)", "Verify if the field is empty, \"როდის გსურთ შეკვეთის მიღება?\" turns red.");
+        Assert.assertEquals(steps.getDateFieldColor(), "rgb(255, 0, 0)", "Verify if the field is empty, \"როდის გსურთ შეკვეთის მიღება?\" turns red.");
     }
 
     @Test(priority = 6)
     @Severity(SeverityLevel.NORMAL)
     @Description("Negative test case: Checking that if the field is empty, \"ელ.ფოსტა\" turns red.")
     public void checkEmailFieldColor(){
-        Assert.assertEquals(emailInputField.getCssValue("border-color"), "rgb(255, 0, 0)", "Verify if the field is empty, \"ელ.ფოსტა\" turns red.");
+        Assert.assertEquals(steps.getEmailFieldColor(), "rgb(255, 0, 0)", "Verify if the field is empty, \"ელ.ფოსტა\" turns red.");
     }
 
     @Test(priority = 7)
     @Severity(SeverityLevel.NORMAL)
-    @Description("Negative test case: Checking that if the field is filled, \"კომპანიის სახელწოდება\" should no longer be red.")
+    @Description("Positive test case: Checking that if the field is filled, \"კომპანიის სახელწოდება\" should no longer be red.")
     public void checkFilledNameFieldColor(){
         steps
                 .fillNameField()
                 .clickSendBtn();
         sleep(400);
-        Assert.assertNotEquals(nameInputField.getCssValue("border-color"), "rgb(255, 0, 0)", "Verify if the field is filled, \"კომპანიის სახელწოდება\" should no longer be red.");
+        Assert.assertNotEquals(steps.getNameFieldColor(), "rgb(255, 0, 0)", "Verify if the field is filled, \"კომპანიის სახელწოდება\" should no longer be red.");
+    }
+
+    @Test(priority = 8)
+    @Severity(SeverityLevel.CRITICAL)
+    @Description("Negative test case: Checking that entering string in the field \"კომპანიის სახელწოდება\" is not possible.")
+    public void checkStringInCode(){
+        steps.inputStringInCode();
+        Assert.assertEquals(steps.getCodeFieldValue(), "", "Verify that entering string in the field \"კომპანიის სახელწოდება\" is not possible.");
+    }
+
+    @Test(priority = 9)
+    @Severity(SeverityLevel.CRITICAL)
+    @Description("Negative test case: Checking that entering less than 11 digits in the field \"კომპანიის სახელწოდება\" is not possible.")
+    public void checkLessDigitsInCode(){
+        steps
+                .inputLessDigitsInCode()
+                .clickSendBtn();
+        sleep(400);
+        Assert.assertEquals(steps.getCodeFieldColor(), "rgb(255, 0, 0)", "Verify tht while entering less than 11 digits in the \"საიდენტიფიკაციო კოდი\" field, it turns red.");
+    }
+
+    @Test(priority = 10)
+    @Severity(SeverityLevel.CRITICAL)
+    @Description("Negative test case: Checking that while entering 12 digits in the field \"კომპანიის სახელწოდება\", 11 characters should be entered in the field.")
+    public void checkTwelveDigitsInCode(){
+        steps.inputTwelveDigitsInCode();
+        Assert.assertEquals(steps.getCodeFieldValue().length(), 11, "Verify that while entering 12 digits in the field \"კომპანიის სახელწოდება\", 11 characters should be entered in the field.");
+    }
+
+    @Test(priority = 11)
+    @Severity(SeverityLevel.NORMAL)
+    @Description("Positive test case: Checking that if the field is filled, \"საიდენტიფიკაციო კოდი\" should no longer be red.")
+    public void checkFilledCodeFieldColor(){
+        steps
+                .inputElevenDigitsInCode()
+                .clickSendBtn();
+        sleep(400);
+        Assert.assertEquals(steps.getCodeFieldValue().length(), 11, "Verify that while entering 11 digits in the field \"საიდენტიფიკაციო კოდი\", 11 characters should be entered in the field.");
+        Assert.assertNotEquals(steps.getCodeFieldColor(), "rgb(255, 0, 0)", "Verify if the field is filled, \"საიდენტიფიკაციო კოდი\" should no longer be red.");
+    }
+
+    @Test(priority = 12)
+    @Severity(SeverityLevel.NORMAL)
+    @Description("Positive test case: Checking that if the field is filled, \"ფაქტიური მისამართი\" should no longer be red.")
+    public void checkFilledAddressFieldColor(){
+        steps
+                .fillAddressField()
+                .clickSendBtn();
+        sleep(400);
+        Assert.assertNotEquals(steps.getAddressFieldColor(), "rgb(255, 0, 0)", "Verify if the field is filled, \"ფაქტიური მისამართი\" should no longer be red.");
+    }
+
+    @Test(priority = 13)
+    @Severity(SeverityLevel.CRITICAL)
+    @Description("Negative test case: Checking that entering string in the field \"საკონტაქტო ნომერი\" is not possible.")
+    public void checkStringInPhone(){
+        steps.inputStringInPhone();
+        Assert.assertEquals(steps.getPhoneFieldValue(), "", "Verify that entering string in the field \"საკონტაქტო ნომერი\" is not possible.");
+    }
+
+    @Test(priority = 14)
+    @Severity(SeverityLevel.CRITICAL)
+    @Description("Negative test case: Checking that entering less than 9 digits in the field \"საკონტაქტო ნომერი\" is not possible.")
+    public void checkLessDigitsInPhone(){
+        steps
+                .inputLessDigitsInPhone()
+                .clickSendBtn();
+        sleep(400);
+        Assert.assertEquals(steps.getPhoneFieldColor(), "rgb(255, 0, 0)", "Verify tht while entering less than 9 digits in the \"საკონტაქტო ნომერი\" field, it turns red.");
+    }
+
+    @Test(priority = 15)
+    @Severity(SeverityLevel.CRITICAL)
+    @Description("Negative test case: Checking that while entering 12 digits in the field \"საკონტაქტო ნომერი\", 9 characters should be entered in the field.")
+    public void checkTwelveDigitsInPhone(){
+        steps.inputTwelveDigitsInPhone();
+        Assert.assertEquals(steps.getPhoneFieldValue().length(), 9, "Verify that while entering 12 digits in the field \"კომპანიის სახელწოდება\", 9 characters should be entered in the field.");
     }
 }
